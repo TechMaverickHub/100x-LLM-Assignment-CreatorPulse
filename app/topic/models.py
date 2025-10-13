286810af2b3fafbd298b473e81e0dc32e2ee678f
Abhiroop <abhiroop1998.dev@gmail.com>
2025-10-13 12:08:46 +0530

    feat: added feature for user to select topic

--- app/topic/models.py
+++ app/topic/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+from app.user.models import User
+
+
 # Create your models here.
 class Topic(models.Model):
 
@@ -11,3 +14,23 @@ class Topic(models.Model):
     updated = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=True)
 
+
+class UserTopic(models.Model):
+    # Link to User
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="topics",  # access all topics of a user: user.topics.all()
+        related_query_name="topic"  # filter users by a specific topic: User.objects.filter(topic=some_topic)
+    )
+
+    # Link to Topic
+    topic = models.ForeignKey(
+        Topic,
+        on_delete=models.CASCADE,
+        related_name="users",  # access all users subscribed to a topic: topic.users.all()
+        related_query_name="user"  # filter topics by a specific user: Topic.objects.filter(user=some_user)
+    )
+
+    class Meta:
+        unique_together = ('user', 'topic')  # optional: prevent duplicate user-topic mappings
--- app/topic/urls.py
+++ app/topic/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from app.topic.views import TopicListAPIView
+from app.topic.views import TopicListAPIView, UserTopicListAPIView, UserTopicUpdateAPIView
 
 urlpatterns = [
     # Authentication
     path("list", TopicListAPIView.as_view(), name="topic-list"),
+
+    path('user-topic-list', UserTopicListAPIView.as_view(), name="user-topic-list"),
+
+    path('user-topic-update', UserTopicUpdateAPIView.as_view(), name="user-topic-create"),
 
     ]
